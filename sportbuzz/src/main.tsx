40ced9abb2f0fe67aefe0242dc9ec9dca9a4b130
AnveshNalimela <nalimelaanvesh@gmail.com>
2024-05-15 21:05:35 +0530

    Second deployment

--- sportbuzz/src/main.tsx
+++ sportbuzz/src/main.tsx
@@ -1,8 +1,8 @@
+import React from "react";
 import ReactDOM from "react-dom/client";
 import App from "./App.tsx";
 import { ThemeProvider } from "./context/theme";
 import "./index.css";
-import React from "react";
 
 ReactDOM.createRoot(document.getElementById("root")!).render(
   <ThemeProvider>
