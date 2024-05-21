98a5a7ad0e8f199485dff9068a6b952e5aaa5e96
AnveshNalimela <nalimelaanvesh@gmail.com>
2024-05-21 19:25:19 +0530

    Removed SportBuZZ

--- sportbuzz/vite-env.d.ts
+++
@@ -1,9 +0,0 @@
-// Create a custom type declaration file (e.g., custom.d.ts) in your project
-
-// custom.d.ts
-
-interface ImportMeta {
-  readonly env: {
-    [key: string]: string | undefined;
-  };
-}
