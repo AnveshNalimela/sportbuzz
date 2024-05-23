b698246512e184305b53727d083c7e82f56abbe9
AnveshNalimela <nalimelaanvesh@gmail.com>
2024-05-23 23:52:05 +0530

    border added

--- src/pages/account/Matches/PMatchList.tsx
+++ src/pages/account/Matches/PMatchList.tsx
@@ -10,7 +10,7 @@ interface PMatchListProps {
 const PMatchList: React.FC<PMatchListProps> = ({ psports, pteams }) => {
   return (
     <div>
-      <div className="h-400 grid gap-4 grid-cols-1 sm:grid-cols-2 lg:grid-cols-5 my-2 rounded p-3">
+      <div className="h-400 grid gap-4 grid-cols-1 sm:grid-cols-2 lg:grid-cols-5 my-2 rounded p-3 border rounded border-gray-200 mt-2 px-5 py-3">
         <PreferedMatchList psports={psports} pteams={pteams} />
       </div>
     </div>
--- src/pages/home/matches/MatchList.tsx
+++ src/pages/home/matches/MatchList.tsx
@@ -4,7 +4,7 @@ import MatchListItems from "./MatchListItems";
 const MatchList: React.FC = () => {
   return (
     <div>
-      <div className="h-400 grid gap-4 grid-cols-1 sm:grid-cols-2 lg:grid-cols-5 my-2 rounded p-3 ">
+      <div className="h-400 grid gap-4 grid-cols-1 sm:grid-cols-2 lg:grid-cols-5 my-2 rounded p-3 border rounded border-gray-200 mt-2  px-5 py-3 ">
         <MatchListItems />
       </div>
     </div>
