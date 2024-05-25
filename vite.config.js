79b001e2fe692797d252f2a73efd050d8478170e
AnveshNalimela <nalimelaanvesh@gmail.com>
2024-05-25 19:55:37 +0530

    app avaliable

--- vite.config.js
+++ vite.config.js
@@ -11,6 +11,41 @@ export default defineConfig({
         enabled: true // For making sure that the PWA is testable from the Local dev environment
       },
       registerType: 'autoUpdate',
+      manifest: {
+        name: "SportBuzz application",
+        short_name: "SportBuzz",
+        icons: [
+          {
+            "src": "/stadium.ico",
+            "sizes": "64x64 32x32 24x24 16x16",
+            "type": "image/x-icon"
+          },
+          {
+            "src": "/favicon-16x16.png",
+            "type": "image/png",
+            "sizes": "16x16"
+          },
+          {
+            "src": "/favicon-32x32.png",
+            "type": "image/png",
+            "sizes": "32x32"
+          },
+          {
+            "src": "/pwa-192x192.png",
+            "type": "image/png",
+            "sizes": "192x192"
+          },
+          {
+            "src": "/pwa-512x512.png",
+            "type": "image/png",
+            "sizes": "512x512",
+            "purpose": "any maskable" // Icon format that ensures that your PWA icon looks great on all Android devices
+          }
+
+
+        ],
+        theme_color: '#AAF',
+      },
 
     }),
   ],
